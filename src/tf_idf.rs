64fb78a3bc5b1f2ecd11f932af6dc6fb145ebe42
Gerd Reiss <gerd@reiss.pro>
2025-01-22 14:36:22 +0100

    some minor changes

--- src/tf_idf.rs
+++ src/tf_idf.rs
@@ -6,9 +6,9 @@ use crate::types::TF;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
-fn compute_tf(term: &str, doc: &TF) -> f32 {
-    let f = *doc.get(term).unwrap_or(&0) as f32;
-    let s = doc.iter().map(|(_, f)| *f).sum::<usize>() as f32;
+fn compute_tf(term: &str, tf: &TF) -> f32 {
+    let f = *tf.get(term).unwrap_or(&0) as f32;
+    let s = tf.iter().map(|(_, f)| *f).sum::<usize>() as f32;
     f / s
 }
 
@@ -18,27 +18,23 @@ fn compute_idf(term: &str, n: usize, df: &DF) -> f32 {
     (n / m).log10()
 }
 
-fn compute_score(phrase: &str, doc: &TF, model: &Model) -> f32 {
+fn compute_score(phrase: &str, tf: &TF, model: &Model) -> f32 {
     let content = phrase.chars().collect::<Vec<_>>();
     Lexer::new(&content)
         .map(|token| {
-            compute_tf(&token, doc) * compute_idf(&token, model.tf_index.len(), &model.df_index)
+            compute_tf(&token, tf) * compute_idf(&token, model.tf_index.len(), &model.df_index)
         })
         .sum()
 }
 
 pub(crate) fn compute_scores(phrase: String, model: &Model) -> HashMap<&PathBuf, f32> {
-    let mut ranks = model
+    model
         .tf_index
         .iter()
         .map(|(path, tf)| (path, compute_score(&phrase, tf, model)))
         .filter(|(_, score)| *score > 0.0)
-        .collect::<Vec<_>>();
-
-    ranks.sort_by(|(_, l), (_, r)| r.total_cmp(l));
-    ranks.clone().into_iter().take(10).for_each(|(p, r)| {
-        println!("{} => {}", p.display(), r);
-    });
-
-    ranks.iter().copied().collect()
+        .collect::<Vec<_>>()
+        .iter()
+        .copied()
+        .collect()
 }
